d655d472259f5bc3e39efdc20760db68ba73cd8f
Baozhu Zuo <zuobaozhu@gmail.com>
2016-10-09 18:45:05 -0700

    add iwpriv backend to juci

---
+++ plugins/juci-openwrt-wireless/rpc/iwpriv.lua
@@ -0,0 +1,10 @@
+#!/usr/bin/lua
+
+-- JUCI Lua Backend Server API
+-- Copyright (c) 2016 Baozhu Zuo <zuobaozhu@gmail.com>. All rights reserved. 
+-- This module is distributed under GNU GPLv3 with additional permission for signed images.
+-- See LICENSE file for more details. 
+
+local ubus = require("orange/ubus"); 
+
+return ubus.bind("iwpriv", { "scan", "config" });  
