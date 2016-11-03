1cfa12a1611f77374be96e121f961ebb2646a8b8
Baozhu Zuo <zuobaozhu@gmail.com>
2016-11-02 23:40:02 -0700

    rewrite rewifi scan, give up call ubus, use the local scan tools

--- plugins/juci-openwrt-wireless/rpc/rewifi.lua
+++ plugins/juci-openwrt-wireless/rpc/rewifi.lua
@@ -6,5 +6,33 @@
 -- See LICENSE file for more details. 
 
 local ubus = require("orange/ubus"); 
+local juci = require("orange/core");
 
-return ubus.bind("rewifi", { "scan", "connect" });  
+
+local function rewifi_scan()
+        local result = {};
+        local lines = {};
+        result["results"] = lines;
+        local stdout = juci.shell("iwpriv ra0 get_site_survey | tail -n+3");
+        for line in stdout:gmatch("[^\r\n]+") do
+                local channel,ssid,bssid,security,siganl,mode,extch,nt,wps,dpid= line:match("([^%s]*)%s*([^%s]*)%s*([^%s]*)%s*([^%s]*)%s*([^%s]*)%s*([^%s]*)%s*([^%s]*)%s*([^%s]*)%s*");
+                local obj = {
+                        ["channel"] = channel,
+                        ["ssid"] = ssid,
+                        ["bssid"] = bssid,
+                        ["security"] = security,
+                        ["siganl"] = siganl,
+                };
+                table.insert(lines, obj);
+        end
+        return result;
+end
+
+local function rewifi_connect(msg)
+        return ubus.call("rewifi", "connect", msg);
+end
+
+return {
+	scan = rewifi_scan,
+	connect = rewifi_connect
+};
